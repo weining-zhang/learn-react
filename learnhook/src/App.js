51870ed54003ea620aab527eb77239b623cd7d5b
welnee <445103710@qq.com>
2021-07-02 22:28:03 +0800

    update

--- learnhook/src/App.js
+++ learnhook/src/App.js
@@ -1,28 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import useMyState from './UseState';
+
+const MyUseState = useMyState;
+
+const themes = {
+  light: {
+    foreground: "#fff222",
+    background: "#f22f22"
+  },
+  dark: {
+    foreground: "black",
+    background: "#ff66ff"
+  }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
+// function Toolbar(props) {
+//   return (
+//     <ThemedButton />
+//   )
+// }
+
+// function ThemedButton() {
+//   const theme = useContext(ThemeContext);
+//   return (
+//     <button style={{background: theme.background, color: theme.foreground}}>
+//       I am styled by theme context!
+//     </button>
+//   )
+// }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [fruit, setFruit] = useState('banana');
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`
-  })
-
-  let handleOrangeClick = () => {
-    setFruit('orange');
-  }
+  // useEffect(() => {
+  //   document.title = `You clicked ${count} times`
+  // })
 
   return (
     <div>
-      <p>you clicked {count} times.</p>
-      <button onClick={() => {setCount(count + 1)}}>
-        Click me
-      </button>
-
-      <p>what fruit I choose is {fruit}.</p>
-      <button onClick={() => {handleOrangeClick()}}>
-        Change fruit
-      </button>
+      <MyUseState />
+      <ThemeContext.Provider value={themes.dark}> {/* 当前的 context 值由上层组件中距离当前组件最近的 <MyContext.Provider> 的 value prop 决定 */}
+        {/* <Toolbar /> */}
+      </ThemeContext.Provider>
     </div>
   )
 }
---
+++ learnhook/src/UseState.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function useMyState() {
+  const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState('banana');
+
+  let handleOrangeClick = () => {
+    setFruit('orange');
+  }q
+
+  return (
+    <div>
+      <p>you clicked {count} times.</p>
+      <button onClick={() => {setCount(count + 1)}}>
+        Click me
+      </button>
+
+      <p>what fruit I choose is {fruit}.</p>
+      <button onClick={() => {handleOrangeClick()}}>
+        Change fruit
+      </button>
+    </div>
+  )
+}
+
+export default useMyState;

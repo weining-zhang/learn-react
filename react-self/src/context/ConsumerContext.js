d4b1e1fe8adcb1226928b35aca73c41e3986134c
welnee <445103710@qq.com>
2021-08-11 15:05:33 +0800

    feat: update context

--- react-self/src/context/ConsumerContext.js
+++ react-self/src/context/ConsumerContext.js
@@ -4,7 +4,11 @@ import { ThemeContext } from './ThemeContext';
 
 const ConsumerButton = () => {
   return (
-    // 这种方法需要一个函数作为子元素（function as a child）。这个函数接收当前的 context 值，并返回一个 React 节点
+    /* 
+      // 在函数式组件中订阅 context
+      // 这种方法需要一个函数作为子元素。这个函数接收当前的 context 值，并返回一个 React 节点。
+      // 传递给函数的 value 值等价于组件树上方离这个 context 最近的 Provider 提供的 value 值。如果没有对应的 Provider，value 参数等同于传递给 createContext() 的 defaultValue
+    */
     <ThemeContext.Consumer>
       {value => <button>{value}</button>}
     </ThemeContext.Consumer>
